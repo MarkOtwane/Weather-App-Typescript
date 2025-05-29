b60e2b825fbf9afa93077cfffadfc901a57f820d
KING DAVID <otwanemark254@gmail.com>
2025-05-30 00:06:57 +0300

    interface users

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+//https://dummyjson.com/
+type Gender = 'Male' | 'female'
+interface Users{
+    id: number,
+    firstName: string,
+    lastName: string,
+    maidenName: string,
+    age: number,
+    gender: Gender,
+    email: string,
+    phone: string,
+    username:string,
+    password: string,
+    birthDate: string,
+    image: string,
+    bloodGroup: string,
+    height: number,
+    weight: number,
+    eyeColor: string,
+    hair:{
+        color: string,
+        type: string,
+    },
+    ip: string,
+    address:{
+        address:string,
+        city: string,
+        state: string,
+        stateCode: string,
+        postalCode:string,
+    
+    coordinates: {
+        lat: number,
+        lng: number
+    },
+    country: string,
+},
+macAddress: string,
+university: string,
+bank: {
+    cardExpire:	string,
+    cardNumber:	string,
+    cardType:	string,
+    currency:	string,
+    iban: string,
+},
+company:{
+    department: string,
+    name:	string,
+    title: string,
+
+    address	: {
+        address	: string,
+        city	: string,
+        state	: string
+        stateCode	: string,
+        postalCode	: string
+        coordinates :{
+            lat:number,
+            lng:number
+        },
+        country: string,
+        },
+    ein: string,
+    ssn: string,
+    userAgent: string,
+
+    crypto: {
+        coin: string,
+        wallet: string,
+        network: string,
+    },
+    role: string
+    }
+}
