08aaf3ffc016a92dc5ad6109e8604c7cd5d44c9a
KING DAVID <otwanemark254@gmail.com>
2025-05-30 08:51:36 +0300

    posts  and the products interface

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 //https://dummyjson.com/
+
+// https://dummyjson.com/users
 type Gender = 'Male' | 'female'
 interface Users{
     id: number,
@@ -34,17 +36,17 @@ interface Users{
         lng: number
     },
     country: string,
-},
-macAddress: string,
-university: string,
-bank: {
+    },
+    macAddress: string,
+    university: string,
+    bank: {
     cardExpire:	string,
     cardNumber:	string,
     cardType:	string,
     currency:	string,
     iban: string,
-},
-company:{
+    },
+    company:{
     department: string,
     name:	string,
     title: string,
@@ -61,6 +63,7 @@ company:{
         },
         country: string,
         },
+    },
     ein: string,
     ssn: string,
     userAgent: string,
@@ -71,5 +74,84 @@ company:{
         network: string,
     },
     role: string
-    }
+}
+
+// https://dummyjson.com/carts
+interface Carts{
+    id: number,
+    products: {
+        id: number,
+        title :string
+        price:	number,
+        quantity:	number,
+        total: number,
+        discountPercentage:	number,
+        discountedTotal: string,
+        thumbnail: string
+    },
+    total: number,
+    discountedTotal: number,
+    userId: number,
+    totalProducts: number,
+    totalQuantity:number,
+}
+
+// https://dummyjson.com/products
+interface Products{
+    id: number,
+    title: string,
+    description: string,
+    category: string,
+    price: number,
+    discountPercentage: number,
+    rating: number,
+    stock: number,
+    tags : {
+        brand: string,
+        sku: string,
+        weight: number
+    },
+    dimensions : {
+        width: number,
+        height: number,
+        depth: number,
+    },
+    warrantyInformation: string,
+    shippingInformation: string,
+    availabilityStatus: string,
+    reviews: {
+        rating: number,
+        comment: string,
+        date: string,
+        reviewerName: string,
+        reviewerEmail: string
+    },
+    returnPolicy: string,
+    minimumOrderQuantity: number,
+    meta: {
+        createdAt: string,
+        updatedAt: string,
+        barcode: string,
+        qrcode: string
+    },
+    images: string,
+     thumbnail: string
+}
+
+interface posts{
+    id: number,
+    title:string,
+    body: string,
+    tags: {
+        0: string,
+        1:string,
+        2:string
+    },
+    reactions: {
+        likes: number,
+        dislikes: string,
+    },
+    views: number,
+    userId: string
+    
 }
